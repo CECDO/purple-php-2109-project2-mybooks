407ebbfd240cce6ddccc4cbcd2255fe0854a1d80
MaiwennBeloy <mcbeloy16@gmail.com>
2021-11-08 11:45:48 +0100

    patch name variable show

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -6,10 +6,10 @@ use App\Model\BooksManager;
 
 class BooksController extends AbstractController
 {
-    public function books(int $id)
+    public function show(int $id)
     {
         $booksManager = new BooksManager();
-        $book = $booksManager->selectOneById($id);
-        return $this->twig->render('Books/show.html.twig', ['book' => $book]);
+        $show = $booksManager->selectOneById($id);
+        return $this->twig->render('Books/show.html.twig', ['show' => $show]);
     }
 }
