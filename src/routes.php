5b98e8cbe1c05534ee1d2d95d257701846517616
MaiwennBeloy <mcbeloy16@gmail.com>
2021-11-12 13:51:14 +0100

    conflicts resolved

--- src/routes.php
+++ src/routes.php
@@ -8,5 +8,5 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
-    'books/book' => ['BooksController', 'book', ['id']],
+    'Books/show' => ['BooksController', 'show', ['id']],
 ];
