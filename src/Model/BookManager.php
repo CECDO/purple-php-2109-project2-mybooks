b8e4851689668a0b01daf8cbdc4bab5a1e1512b2
Alexandre Grué <alexandre.grue@hotmail.com>
2021-11-13 10:42:31 +0100

    first commit

--- src/Model/BookManager.php
+++ src/Model/BookManager.php
@@ -7,4 +7,9 @@ use App\Model\AbstractManager;
 class BookManager extends AbstractManager
 {
     public const TABLE = "book";
+
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
 }
